d13d512138b1a50e53bc1448c365526d3cb1f69e
Rayon Pinnock <pinnock.mail@gmail.com>
2022-07-05 13:50:23 -0400

    Complete s03-023
    Functions and Handlers

--- main.go
+++ main.go
@@ -6,14 +6,22 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!\n")
-		if err != nil {
-			log.Println(err)
-		}
-		log.Printf("Wrote %d bytes in response\n", n)
-	})
+const port string = ":8080"
 
-	http.ListenAndServe(":8080", nil)
+// Home is the handler for the Home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the Home page\n")
+}
+
+// Abou is the handler for the About page
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the About page\n")
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	log.Printf("Starting web server on port %s\n", port)
+	http.ListenAndServe(port, nil)
 }
