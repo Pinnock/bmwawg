002ee3788af23766616df69608178d75c2794747
Rayon Pinnock <pinnock.mail@gmail.com>
2022-07-06 20:47:36 -0400

    Complete lesson 39
    Using chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,17 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/pinnock/bmwawg/pkg/config"
 	"github.com/pinnock/bmwawg/pkg/handlers"
 )
 
 func routes(c *config.AppConfig) http.Handler {
-	mux := pat.New()
 	h := handlers.NewHandlers(c)
+
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
 	mux.Get("/", http.HandlerFunc(h.Home))
 	mux.Get("/about", http.HandlerFunc(h.About))
 	return mux
